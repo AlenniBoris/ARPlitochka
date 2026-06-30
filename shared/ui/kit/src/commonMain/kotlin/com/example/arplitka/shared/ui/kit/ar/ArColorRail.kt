c339c8d5e8fd061fb45f2837c6afd4bd14c3f1ad
Alennikov Boris <70971570+AlenniBoris@users.noreply.github.com>
2026-06-30 22:20:07 +0300

    1

--- shared/ui/kit/src/commonMain/kotlin/com/example/arplitka/shared/ui/kit/ar/ArColorRail.kt
+++ shared/ui/kit/src/commonMain/kotlin/com/example/arplitka/shared/ui/kit/ar/ArColorRail.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
--- shared/ui/kit/src/commonMain/kotlin/com/example/arplitka/shared/ui/kit/ar/ArTilePickerBottomSheet.kt
+++ shared/ui/kit/src/commonMain/kotlin/com/example/arplitka/shared/ui/kit/ar/ArTilePickerBottomSheet.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
